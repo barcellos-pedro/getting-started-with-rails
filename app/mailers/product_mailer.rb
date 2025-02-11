7240bc04fd4cd2341901ccbf34cd3f053b79f549
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2025-02-10 22:25:13 -0300

    Add tests

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -1,10 +1,15 @@
 class ProductMailer < ApplicationMailer
   def in_stock
-    subscriber = params[:subscriber]
     @greeting = "Good News!"
     @product = params[:product]
     @token = subscriber.generate_token_for(:unsubscribe)
 
     mail to: subscriber.email
+  end
+
+  private
+
+  def subscriber
+    params[:subscriber]
   end
 end
--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -2,10 +2,20 @@ require "test_helper"
 
 class ProductMailerTest < ActionMailer::TestCase
   test "in_stock" do
-    mail = ProductMailer.in_stock
-    assert_equal "In stock", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = ProductMailer.with(mail_params).in_stock
+
+    assert_equal "Product available!", mail.subject
+    assert_equal [ "david@example.com" ], mail.to
+    assert_equal "pedro@gmail.com", mail.from.first
+    assert_match "Good News!", mail.body.encoded
+  end
+
+  private
+
+  def mail_params
+    {
+      product: products(:tshirt),
+      subscriber: subscribers(:david)
+    }
   end
 end
